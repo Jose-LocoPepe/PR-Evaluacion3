aed807edeac126fcddcbb586965bb3423d834b56
Jose Bautista <jose.bautista@alumnos.ucn.cl>
2025-01-09 15:14:48 -0300

    feat: Nuevo Contrato and index

--- WEB-Personas/Pages/Agregar/NuevoContrato.cshtml.cs
+++ WEB-Personas/Pages/Agregar/NuevoContrato.cshtml.cs
@@ -1,12 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WEB_Personas;
 
 namespace MyApp.Namespace
 {
     public class NuevoContratoModel : PageModel
     {
+        public List<Persona> Personas { get; set; } = new List<Persona>();
+        public List<Obra> Obras { get; set; } = new List<Obra>();
         public void OnGet()
         {
+            ObraspersonasContext context = new ObraspersonasContext();
+            Personas = context.Personas.ToList();
+            Obras = context.Obras.ToList();
         }
+        public void OnPost(int idPersona, string nombreObra, DateTime fechaInicio, DateTime fechaTermino, int idObra, Boolean vigente)
+        {
+            ObraspersonasContext context = new ObraspersonasContext();
+            Contrato contrato = new Contrato();
+            contrato.IdPersona = idPersona;
+            contrato.NumContrato = nombreObra;
+            contrato.FechaInicio = fechaInicio;
+            contrato.FechaFin = fechaTermino;
+            contrato.IdObra = idObra;
+            contrato.Vigencia = vigente;
+            context.Contratos.Add(contrato);
+            context.SaveChanges();
+            Response.Redirect("/Listar/ListarContratos");
+        }
+
     }
 }
--- WEB-Personas/Pages/Listar/ListarContratos.cshtml.cs
+++ WEB-Personas/Pages/Listar/ListarContratos.cshtml.cs
@@ -1,12 +1,55 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WEB_Personas;
 
 namespace MyApp.Namespace
 {
     public class ListarContratosModel : PageModel
     {
+        // Asegúrate de que esta propiedad sea pública
+        public List<PersonaConContratosVigentes> PersonasConContratos { get; set; } = new List<PersonaConContratosVigentes>();
+
         public void OnGet()
         {
+            using var context = new ObraspersonasContext();
+
+            PersonasConContratos = context.Personas
+                .Include(p => p.Contratos)
+                .ThenInclude(c => c.IdObraNavigation)
+                .Where(p => p.Contratos.Any(c => c.Vigencia))
+                .Select(p => new PersonaConContratosVigentes
+                {
+                    Id = p.Id,
+                    Nombre = p.Nombre,
+                    Rut = p.Rut,
+                    Contratos = p.Contratos
+                        .Where(c => c.Vigencia)
+                        .Select(c => new ContratoConObra
+                        {
+                            NumContrato = c.NumContrato,
+                            FechaInicio = c.FechaInicio,
+                            FechaFin = c.FechaFin,
+                            NombreObra = c.IdObraNavigation.Nombre
+                        })
+                        .ToList()
+                })
+                .ToList();
         }
+    }
+
+    public class PersonaConContratosVigentes
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Rut { get; set; }
+        public List<ContratoConObra> Contratos { get; set; }
+    }
+
+    public class ContratoConObra
+    {
+        public string NumContrato { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime? FechaFin { get; set; }
+        public string NombreObra { get; set; }
     }
 }
