4127c1afa6f2a37d9ec5d4ed39d6d42f287a538b
Ertinho <105566974+Ertinho@users.noreply.github.com>
2025-01-10 01:15:48 -0300

    feat(WEB-Mat): list the works and their materials

--- WEB-Materiales/Pages/List/AdministrarMaterialEnObra.cshtml.cs
+++ WEB-Materiales/Pages/List/AdministrarMaterialEnObra.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using WEB_Materiales.Models;
 namespace MyApp.Namespace
 {
     public class AdministrarMaterialEnObraModel : PageModel
     {
-        public void OnGet()
+        public Obra Obra { get; set; }
+        public List<Movimiento> Movimientos { get; set; }
+
+        public void OnGet(int id)
         {
+            MaterialContext context = new ();
+            
+            Obra = context.Obras.Find(id);
+            Movimientos = context.Movimientos.Where(m => m.IdObra == id).Include(m => m.IdMaterialNavigation).ToList();
         }
     }
 }
--- WEB-Materiales/Pages/List/ListaObras.cshtml.cs
+++ WEB-Materiales/Pages/List/ListaObras.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using WEB_Materiales.Models;
 namespace MyApp.Namespace
 {
     public class ListaObrasModel : PageModel
     {
+        public List<Obra> Obras { get; set; }
         public void OnGet()
         {
+            MaterialContext context = new ();
+            Obras = context.Obras.ToList();
         }
     }
 }
