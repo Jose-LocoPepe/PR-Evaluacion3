337f54431ffe4ecd8aa8ef45b987ba7f90332929
Ertinho <105566974+Ertinho@users.noreply.github.com>
2025-01-10 12:56:45 -0300

    feat(Pages): show materials in a construction work

--- WEB-Materiales/Pages/List/AdministrarMaterialEnObra.cshtml.cs
+++ WEB-Materiales/Pages/List/AdministrarMaterialEnObra.cshtml.cs
@@ -8,6 +8,8 @@ namespace MyApp.Namespace
     {
         public Obra Obra { get; set; }
         public List<Movimiento> Movimientos { get; set; }
+        public List<Material> Materiales { get; set; }
+        public string ErrorMessage { get; set; }
 
         public void OnGet(int id)
         {
@@ -18,6 +20,57 @@ namespace MyApp.Namespace
                 .Where(m => m.IdObra == id)
                 .Include(m => m.IdMaterialNavigation)
                 .ToList();
+
+            if (Obra != null)
+            {
+                Movimientos = Obra.Movimientos.ToList();
+            }
+            Materiales = context.Materiales.ToList();
+        }
+
+        public void OnPost(int ObraId, int materialId, int cantidad)
+        {
+            try
+            {
+                // if (cantidad <= 0)
+                // {
+                //     ErrorMessage = "La cantidad debe ser un número positivo.";
+                //     OnGet(ObraId); // Reload data
+                //     return;
+                // }
+
+                MaterialContext context = new ();
+
+                // reducir material en inventario
+                Material material = context.Materiales.Find(materialId);
+
+                // if (material.CantidadTotal < cantidad)
+                // {
+                //     ErrorMessage = "No hay suficiente material en inventario.";
+                //     OnGet(ObraId); // Reload data
+                // }
+                material.CantidadTotal -= cantidad;
+                // context.SaveChanges();
+
+                Movimiento movimiento = new ()
+                {
+                    IdObra = ObraId,
+                    IdMaterial = materialId,
+                    Cantidad = cantidad,
+                    IdObraNavigation = context.Obras.Find(ObraId),
+                    IdMaterialNavigation = material
+                };
+                context.Movimientos.Add(movimiento);
+                context.SaveChanges();
+
+                // Response.Redirect($"/List/AdministrarMaterialEnObra?id={ObraId}");
+                Response.Redirect("/Index");
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"An error occurred while adding the client: {ex.Message}");
+                throw;
+            }
         }
     }
 }
