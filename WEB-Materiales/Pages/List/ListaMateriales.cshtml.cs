084fe2480910afbeaace47411b81fcc10cdb7ff0
Ertinho <105566974+Ertinho@users.noreply.github.com>
2025-01-09 21:08:56 -0300

    feat(Pages): add list of materials

---
+++ WEB-Materiales/Pages/List/ListaMateriales.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyApp.Namespace
+{
+    public class ListaMaterialesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
